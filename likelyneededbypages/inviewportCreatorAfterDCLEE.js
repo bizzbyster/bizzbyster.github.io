0c7ed27bb6bbac9dfb46872123abfee1b3a33775
bizzbyster <bizzbyster@gmail.com>
2017-10-22 09:36:47 -0400

    Update inviewportCreatorAfterDCLEE.js

--- likelyneededbypages/inviewportCreatorAfterDCLEE.js
+++ likelyneededbypages/inviewportCreatorAfterDCLEE.js
@@ -1,4 +1,14 @@
+function sleep(milliseconds){
+    var start = new Date().getTime();
+    var keep_looping = true;
+    while (keep_looping) {
+      if ((new Date().getTime() - start)> milliseconds) {
+        keep_looping = false;
+      }
+    }
+}
 (function() {
     'use strict';
+    sleep(1500);
     callToDateToGenerateBreadcrumb();
 })();
