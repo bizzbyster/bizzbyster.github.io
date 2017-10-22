c981a9522931abebf8c325af437dd4f01a41084b
bizzbyster <bizzbyster@gmail.com>
2017-10-22 09:25:46 -0400

    Update inviewportCreatorAfterDCLEE.js

--- likelyneededbypages/inviewportCreatorAfterDCLEE.js
+++ likelyneededbypages/inviewportCreatorAfterDCLEE.js
@@ -1,8 +1,4 @@
 (function() {
     'use strict';
     callToDateToGenerateBreadcrumb();
-    var imageNode = document.createElement('img');
-    imageNode.src = 'sparrow.png?inviewport-jsloaded';
-    var inviewportDiv = document.getElementById('inviewport-images');
-    inviewportDiv.appendChild(imageNode);
 })();
