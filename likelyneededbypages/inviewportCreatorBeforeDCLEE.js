7b9b64b459324808f9dc459ac37df06a97b0dbb6
bizzbyster <bizzbyster@gmail.com>
2017-10-22 09:32:53 -0400

    Update inviewportCreatorBeforeDCLEE.js

--- likelyneededbypages/inviewportCreatorBeforeDCLEE.js
+++ likelyneededbypages/inviewportCreatorBeforeDCLEE.js
@@ -1,9 +1,19 @@
+function sleep(milliseconds){
+    var start = new Date().getTime();
+    var keep_looping = true;
+    while (keep_looping) {
+      if ((new Date().getTime() - start)> milliseconds) {
+        keep_looping = false;
+      }
+    }
+  }
 (function() {
     'use strict';
     var imageNode = document.createElement('img');
     imageNode.src = 'sparrow.png?inviewport-jsloaded';
     var inviewportDiv = document.getElementById('inviewport-images');
     inviewportDiv.appendChild(imageNode);
+    sleep(1500);
     var scriptLoader = document.createElement('script');
     scriptLoader.type = 'application/javascript';
     scriptLoader.src = 'inviewportCreatorAfterDCLEE.js';
